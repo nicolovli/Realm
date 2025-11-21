3dd34154e122163f60304b5daa605099066d069b
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-11-21 13:20:30 +0100

    feat: add mobile placeholder for search

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -83,7 +83,9 @@ export const Header = () => {
 
         {/* Mobile icons only visible in reponsivive small size */}
         <section className="flex flex-row gap-3 w-full md:hidden items-center">
-          <SearchBar />
+          <SearchBar
+                      placeholder="Search"
+ />
           <MobileSheetMenu
             navigation={navigation}
             dialogControls={loginDialogControls}
